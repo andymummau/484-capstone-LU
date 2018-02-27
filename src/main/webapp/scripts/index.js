17e99070aa7bacd7dc2b30cd4facf521adacf529
andymummau <andymummau@gmail.com>
2018-02-26 22:17:21 -0500

    Visual formatting tweaks for desktop Cleaned up code Click to load sentence content

--- src/main/webapp/scripts/index.js
+++ src/main/webapp/scripts/index.js
@@ -1,8 +1,8 @@
 angular
     .module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'ngMdIcons'])
 
+//Color Theming config
 .config(function($mdThemingProvider) {
-
   $mdThemingProvider.theme('default')
     .primaryPalette('blue', {
       'default': '800',
@@ -15,61 +15,48 @@ angular
     });
 })
 
+//Primary controller
 .controller('AppCtrl', function($scope, $timeout, $mdSidenav, $mdDialog, $http) {
-    $scope.toggleLeft = buildToggler('left');
-    
-    function buildToggler(componentId){
-        return function(){
-            $mdSidenav(componentId).toggle();
-        };
-    }
-    $scope.menu = [
-    {
-      link : '',
-      title: 'Change Language',
-      icon: 'language'
-    },
-    {
-      link : '#',
-      title: 'Upload',
-      icon: 'playlist_add'
-    }
-  ];
-    $scope.selectLanguage = function(ev) {
+$scope.selectLanguage = function(ev) {
     $mdDialog.show({
-      controller: DialogController,
-      templateUrl: "../templates/langSelect.html",
-      targetEvent: ev,
+        controller: DialogController,
+        templateUrl: "../templates/langSelect.html",
+        targetEvent: ev,
+        scope: angular.extend($scope.$new(), { close: function() {$mdDialog.cancel();} }),
     })
     /*.then(function(answer) {
       $scope.alert = 'You said the information was "' + answer + '".';
     }, function() {
       $scope.alert = 'You cancelled the dialog.';
     });*/
-  };
-    $scope.viewTranslation = function(ev) {
+};
+$scope.viewTranslation = function(ev) {
     $mdDialog.show({
-      controller: DialogController,
-      templateUrl: "../templates/translationSlider.html",
-      targetEvent: ev,
+        controller: DialogController,
+        templateUrl: "../templates/translationSlider.html",
+        targetEvent: ev,
     })
     /*.then(function(answer) {
       $scope.alert = 'You said the information was "' + answer + '".';
     }, function() {
       $scope.alert = 'You cancelled the dialog.';
     });*/
-  };
-    function DialogController($scope, $mdDialog) {
-  $scope.hide = function() {
-    $mdDialog.hide();
-  }
+};
+function DialogController($scope, $mdDialog) {
+    $scope.hide = function() {
+        $mdDialog.hide();
+    }
     $scope.cancel = function() {
-    $mdDialog.cancel();
-  };
-  $scope.answer = function(answer) {
-    $mdDialog.hide(answer);
-  };
-    }
+        $mdDialog.cancel();
+    };
+    $scope.answer = function(answer) {
+        $mdDialog.hide(answer);
+    };
+};
+//Temporay code to demonstrate proof-of-concept    
+$scope.printSentence = function() {
+    $scope.chips = ["The","quick","blue","fox","jumps","over","the","lazy","dog"];
+}
    //$scope.chips = $http.get('api/visitors');
 
    /*$http.get("api/visitors")
@@ -81,72 +68,5 @@ angular
        console.log("The request failed with response " + response + " and status code " + status);
      });*/
 
-   //console.log($scope.chips);
-   $scope.chips = ["The","quick","blue","fox","jumps","over","the","lazy","dog"];
+   //console.log($scope.chips);  
 });
-
-/*angular
-    .module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
-
-.controller('AppCtrl', function($scope, $timeout, $mdSidenav, $log) {
-    $scope.toggleLeft = buildDelayedToggler('left');
-
-};*/
-    /**
-     * Supplies a function that will continue to operate until the
-     * time is up.
-     */
-   /* function debounce(func, wait, context) {
-      var timer;
-
-      return function debounced() {
-        var context = $scope,
-            args = Array.prototype.slice.call(arguments);
-        $timeout.cancel(timer);
-        timer = $timeout(function() {
-          timer = undefined;
-          func.apply(context, args);
-        }, wait || 10);
-      };
-    }*/
-
-    /**
-     * Build handler to open/close a SideNav; when animation finishes
-     * report completion in console
-     */
-    /*function buildDelayedToggler(navID) {
-      return debounce(function() {
-        // Component lookup should always be available since we are not using `ng-if`
-        $mdSidenav(navID)
-          .toggle()
-          .then(function () {
-            $log.debug("toggle " + navID + " is done");
-          });
-      }, 200);
-    }
-
-    function buildToggler(navID) {
-      return function() {
-        // Component lookup should always be available since we are not using `ng-if`
-        $mdSidenav(navID)
-          .toggle()
-          .then(function () {
-            $log.debug("toggle " + navID + " is done");
-          });
-      };
-    }
-  })
-  .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-    $scope.close = function () {
-      // Component lookup should always be available since we are not using `ng-if`
-      $mdSidenav('left').close()
-        .then(function () {
-          $log.debug("close LEFT is done");
-        });
-
-    };
-  });*/
-/**
-Copyright 2016 Google Inc. All Rights Reserved. 
-Use of this source code is governed by an MIT-style license that can be foundin the LICENSE file at http://material.angularjs.org/HEAD/license.
-**/
