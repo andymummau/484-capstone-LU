5998ff36d3711fc123e841faf312a4ffb9042b44
Noah <nbjohnso202@gmail.com>
2018-02-27 17:11:06 -0500

    Added demo output slider

--- src/main/webapp/scripts/index.js
+++ src/main/webapp/scripts/index.js
@@ -1,5 +1,5 @@
 angular
-    .module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'ngMdIcons'])
+    .module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'ngMdIcons', 'ngAnimate'])
 
 //Color Theming config
 .config(function($mdThemingProvider) {
@@ -87,4 +87,73 @@ $scope.clearSentence = function() {
      });*/
 
    //console.log($scope.chips);  
-});
+})
+
+//Animated Slider
+    .controller('MainCtrl', function ($scope) {
+        $scope.slides = [
+            {image: 'https://media.giphy.com/media/26FL1Z4aQQggwu57G/giphy.gif', description: 'Image 00'},
+            {image: 'https://media.giphy.com/media/3o6Zt75NDGTAIeydck/giphy.gif', description: 'Image 01'},
+            {image: 'https://media.giphy.com/media/l4q7X9WikgtfBKmCQ/giphy.gif', description: 'Image 02'},
+            {image: 'https://media.giphy.com/media/l0MYtTptyL8h88UHm/giphy.gif', description: 'Image 03'},
+            {image: 'https://media.giphy.com/media/l0HlBGjKUV8KJxDoc/giphy.gif', description: 'Image 04'}
+        ];
+
+        $scope.direction = 'left';
+        $scope.currentIndex = 0;
+
+        $scope.setCurrentSlideIndex = function (index) {
+            $scope.direction = (index > $scope.currentIndex) ? 'left' : 'right';
+            $scope.currentIndex = index;
+        };
+
+        $scope.isCurrentSlideIndex = function (index) {
+            return $scope.currentIndex === index;
+        };
+
+        $scope.prevSlide = function () {
+            $scope.direction = 'left';
+            $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
+        };
+
+        $scope.nextSlide = function () {
+            $scope.direction = 'right';
+            $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
+        };
+    })
+    .animation('.slide-animation', function () {
+        return {
+            beforeAddClass: function (element, className, done) {
+                var scope = element.scope();
+
+                if (className == 'ng-hide') {
+                    var finishPoint = element.parent().width();
+                    if(scope.direction !== 'right') {
+                        finishPoint = -finishPoint;
+                    }
+                    TweenMax.to(element, 0.5, {left: finishPoint, onComplete: done });
+                }
+                else {
+                    done();
+                }
+            },
+            removeClass: function (element, className, done) {
+                var scope = element.scope();
+
+                if (className == 'ng-hide') {
+                    element.removeClass('ng-hide');
+
+                    var startPoint = element.parent().width();
+                    if(scope.direction === 'right') {
+                        startPoint = -startPoint;
+                    }
+
+                    TweenMax.fromTo(element, 0.5, { left: startPoint }, {left: 0, onComplete: done });
+                }
+                else {
+                    done();
+                }
+            }
+        };
+    });
+
