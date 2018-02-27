f0b7a8c556ab84c152c4ed19172aec65a8f59aff
andymummau <andymummau@gmail.com>
2018-02-27 17:45:27 -0500

    New AngularJS slider

--- src/main/webapp/scripts/index.js
+++ src/main/webapp/scripts/index.js
@@ -95,74 +95,143 @@ $scope.disableTrans = function() {
        console.log("The request failed with response " + response + " and status code " + status);
      });*/
 
-   //console.log($scope.chips);  
+   //console.log($scope.chips);
+        $scope.index = 0;
+
+    
+//Slider Stuff
+  $scope.images = [
+    'https://media.giphy.com/media/26FL1Z4aQQggwu57G/giphy.gif',
+    'https://media.giphy.com/media/3o6Zt75NDGTAIeydck/giphy.gif',
+    'https://media.giphy.com/media/l4q7X9WikgtfBKmCQ/giphy.gif'
+  ];
+  
+  // callbacks for change in slides
+  $scope.updateTsPrevious = function() {
+    $scope.tsPrevious = +new Date();
+  };
+  $scope.updateTsNext = function() {
+    $scope.tsNext = +new Date();
+  };
+
 })
 
 //Animated Slider
-    .controller('MainCtrl', function ($scope) {
-        $scope.slides = [
-            {image: 'https://media.giphy.com/media/26FL1Z4aQQggwu57G/giphy.gif', description: 'Image 00'},
-            {image: 'https://media.giphy.com/media/3o6Zt75NDGTAIeydck/giphy.gif', description: 'Image 01'},
-            {image: 'https://media.giphy.com/media/l4q7X9WikgtfBKmCQ/giphy.gif', description: 'Image 02'},
-            {image: 'https://media.giphy.com/media/l0MYtTptyL8h88UHm/giphy.gif', description: 'Image 03'},
-            {image: 'https://media.giphy.com/media/l0HlBGjKUV8KJxDoc/giphy.gif', description: 'Image 04'}
-        ];
+.directive('wallopSlider', function () {
+    return {
+      template: '<div class="wallop-slider {{animationClass}}"><ul class="wallop-slider__list"><li class="wallop-slider__item {{itemClasses[$index]}}" ng-repeat="i in images"><img src="{{i}}"></li></ul><button ng-show="images.length>1" class="st-button wallop-slider__btn wallop-slider__btn--previous btn btn--previous" ng-disabled="prevDisabled" ng-click="onPrevButtonClicked()">Previous</button><button ng-show="images.length>1" class="st-button wallop-slider__btn wallop-slider__btn--next btn btn--next" ng-disabled="nextDisabled" ng-click="onNextButtonClicked()">Next</button></div>',
+      restrict: 'EA',
+      transclude: true,
+      replace: false,
+      scope: {
+        images: '=',
+        animation: '@',
+        currentItemIndex: '=',
+        onNext: '&',
+        onPrevious: '&'
+      },
+      controller: function($scope, $timeout) {
 
-        $scope.direction = 'left';
-        $scope.currentIndex = 0;
+        $scope.itemClasses = [];
 
-        $scope.setCurrentSlideIndex = function (index) {
-            $scope.direction = (index > $scope.currentIndex) ? 'left' : 'right';
-            $scope.currentIndex = index;
+        $scope.$watch('images', function(images) {
+          if (images.length) {
+            _goTo(0);
+          }
+        });
+
+        $scope.$watch('itemClasses', function(itemClasses) {
+          console.log('itemClasses', itemClasses);
+        });
+
+        // set animation class corresponding to animation defined in CSS. e.g. rotate, slide
+        if ($scope.animation) {
+          $scope.animationClass = 'wallop-slider--' + $scope.animation;
+        }
+
+        var _displayOptions = {
+          btnPreviousClass: 'wallop-slider__btn--previous',
+          btnNextClass: 'wallop-slider__btn--next',
+          itemClass: 'wallop-slider__item',
+          currentItemClass: 'wallop-slider__item--current',
+          showPreviousClass: 'wallop-slider__item--show-previous',
+          showNextClass: 'wallop-slider__item--show-next',
+          hidePreviousClass: 'wallop-slider__item--hide-previous',
+          hideNextClass: 'wallop-slider__item--hide-next'
         };
 
-        $scope.isCurrentSlideIndex = function (index) {
-            return $scope.currentIndex === index;
-        };
-
-        $scope.prevSlide = function () {
-            $scope.direction = 'left';
-            $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
-        };
-
-        $scope.nextSlide = function () {
-            $scope.direction = 'right';
-            $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
-        };
-    })
-    .animation('.slide-animation', function () {
-        return {
-            beforeAddClass: function (element, className, done) {
-                var scope = element.scope();
-
-                if (className == 'ng-hide') {
-                    var finishPoint = element.parent().width();
-                    if(scope.direction !== 'right') {
-                        finishPoint = -finishPoint;
-                    }
-                    TweenMax.to(element, 0.5, {left: finishPoint, onComplete: done });
-                }
-                else {
-                    done();
-                }
-            },
-            removeClass: function (element, className, done) {
-                var scope = element.scope();
-
-                if (className == 'ng-hide') {
-                    element.removeClass('ng-hide');
-
-                    var startPoint = element.parent().width();
-                    if(scope.direction === 'right') {
-                        startPoint = -startPoint;
-                    }
-
-                    TweenMax.fromTo(element, 0.5, { left: startPoint }, {left: 0, onComplete: done });
-                }
-                else {
-                    done();
-                }
+        function updateClasses() {
+          if ($scope.itemClasses.length !== $scope.images.length) {
+            $scope.itemClasses = [];
+            for (var i=0; i<$scope.images.length; i++) {
+              $scope.itemClasses.push('');
             }
-        };
-    });
+          }
+        }
+        function _nextDisabled() {
+          console.log('$scope.currentItemIndex', $scope.currentItemIndex, $scope.images.length);
 
+          return ($scope.currentItemIndex + 1) === $scope.images.length;
+        }
+        function _prevDisabled() {
+          return !$scope.currentItemIndex;
+        }
+        function _updatePagination() {
+          $scope.nextDisabled = _nextDisabled();
+          $scope.prevDisabled = _prevDisabled();
+        }
+        function _clearClasses() {
+          for (var i=0; i<$scope.images.length; i++) {
+            $scope.itemClasses[i] = '';
+          }
+
+        }
+
+        // go to slide
+        function _goTo(index) {
+          console.log('_goTo', index);
+          if (index >= $scope.images.length || index < 0 || index === $scope.currentItemIndex) {
+
+            if (!index) {
+              $scope.itemClasses[0] = _displayOptions.currentItemClass;
+            }
+            return;
+          }
+
+          _clearClasses();
+
+          $scope.itemClasses[$scope.currentItemIndex] = (index > $scope.currentItemIndex) ? _displayOptions.hidePreviousClass : _displayOptions.hideNextClass;
+
+          var currentClass = (index > $scope.currentItemIndex) ? _displayOptions.showNextClass : _displayOptions.showPreviousClass;
+          $scope.itemClasses[index] = _displayOptions.currentItemClass + ' ' + currentClass;
+
+          $scope.currentItemIndex = index;
+
+          _updatePagination();
+
+        }
+
+        // button event handlers
+        // consider using the ng-tap directive to remove delay
+        $scope.onPrevButtonClicked = function () {
+          _goTo($scope.currentItemIndex - 1);
+        };
+        $scope.onNextButtonClicked = function () {
+          _goTo($scope.currentItemIndex + 1);
+        };
+        
+        $scope.$watch('currentItemIndex', function(newVal, oldVal) {
+          if (oldVal > newVal) {
+            if (typeof $scope.onPrevious === 'function') {
+              $scope.onPrevious();
+            }
+          } else {
+            if (typeof $scope.onNext === 'function') {
+              $scope.onNext();
+            }
+          }
+        });
+
+      }
+    };
+});
